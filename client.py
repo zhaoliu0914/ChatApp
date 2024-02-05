4c59f529796428d6defd410a8307df3702e374a5
Zhao Liu <liu.9553@osu.edu>
2024-02-04 22:06:17 -0500

    implement server and client

--- client.py
+++ client.py
@@ -5,27 +5,129 @@ import logging
 import configparser
 import threading
 
+
+# Server configuration
+server_ip = "192.168.1.132"
+server_port = 8888
+server_address = (server_ip, server_port)
+
+# client socket
 client = None
+
+# communication json type:
+LOGIN = "login"
+LIST_USERS = "list_users"
+BROADCAST = "broadcast"
+GROUP_SENDING = "group_sending"
+DIRECT_MESSAGE = "direct_message"
+
+SUCCESS = "success"
+
+# user infor
+user_name = None
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(socket.gethostname())
+
+
+def help_information():
+    print("[show] : display all help/available information")
+    print("[list] : display all active users in the server")
+    print("[dm:<receiver>]<message> : direct message <message> to <receiver>")
+    print("<message> : group message <message> to all active users in the server")
 
 
 def receiving_message():
     while True:
+        # receive response from server
         server_data = client.recv(1024)
-        server_data.decode(encoding="utf-8")
+        server_data = server_data.decode(encoding="utf-8")
+        server_json = json.loads(server_data)
+        response_type = server_json["type"]
+        response_status = server_json["status"]
+        response_sender = server_json["sender"]
+        response_content = server_json["content"]
 
-        print(f"Receive from {server_ip}: {server_data}")
+        print(f"{response_sender}: {response_content}")
 
 
 def sending_message():
     for line in sys.stdin:
         line = line.strip()
-        line = line.encode(encoding="utf-8")
-        client.send(line)
+
+        if len(line) == 0:
+            print("You cannot send empty message to the Server!")
+            continue
+
+        elif line == "[show]":
+            help_information()
+
+        elif line == "[list]":
+            try:
+                message_json = format_communication_json(LIST_USERS, None, line)
+                client.send(message_json)
+            except:
+                print("There some errors occurs.")
+                print("Please exit the program and then restart again!")
+
+        elif line.startswith("[dm:"):
+            try:
+                # get the receiver and content from input string.
+                line_str = str.split("[dm:")
+                #print(f"strarr[0] = {line_str[0]}")
+                #print(f"strarr[1] = {line_str[1]}")
+                receiver_content = line_str[1].split("]")
+                #print(f"str_message[0] = {receiver_content[0]}")
+                #print(f"str_message[1] = {receiver_content[1]}")
+
+                receiver = receiver_content[0]
+                content = receiver_content[1]
+
+                # send message to server
+                message_json = format_communication_json(DIRECT_MESSAGE, receiver, content)
+                client.send(message_json)
+            except:
+                print("You have entered wrong format message.")
+                print("Please enter again!")
+                help_information()
+                continue
+        else:
+            try:
+                # send message to server
+                message_json = format_communication_json(GROUP_SENDING, None, line)
+                client.send(message_json)
+            except:
+                print("There some errors occurs.")
+                print("Please exit the program and then restart again!")
+
+
+def format_communication_json(type, receiver, content):
+    if type == LOGIN:
+        message = {"type": type,
+                   "sender": content,
+                   "content": content}
+
+    elif type == DIRECT_MESSAGE:
+        message = {"type": type,
+                   "sender": user_name,
+                   "receiver": receiver,
+                   "content": content}
+
+    else:
+        message = {"type": type,
+                   "sender": user_name,
+                   "content": content}
+
+    message_json = json.dumps(message)
+    message_json = message_json.encode(encoding="utf-8")
+
+    return message_json
 
 
 if __name__ == '__main__':
-    server_ip = "172.27.67.88"
-    server_address = (server_ip, 8888)
+
+    print(f"Host Name: {host_name}")
+    print(f"Host IP: {host_ip}")
+    help_information()
 
     # Set up socket
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,6 +135,38 @@ if __name__ == '__main__':
 
     print(f"Connected to Server address: {server_address}")
     logging.info(f"Connected to Server address: {server_address}")
+
+    # Verify username
+    print(f"Please enter your username to login to server({server_ip}): ")
+    for line in sys.stdin:
+        line = line.strip()
+
+        if len(line) == 0:
+            print("username cannot be empty!")
+            print(f"Please enter your username to login to server({server_ip}): ")
+            continue
+
+        # send username to server
+        message_json = format_communication_json(LOGIN, None, line)
+        client.send(message_json)
+
+        # receive response from server
+        server_data = client.recv(1024)
+        server_data = server_data.decode(encoding="utf-8")
+        server_json = json.loads(server_data)
+        response_type = server_json["type"]
+        response_status = server_json["status"]
+
+        # check the status returned by server
+        if response_status == SUCCESS:
+            user_name = line
+            print("You have successfully login to Server")
+            break
+        else:
+            user_name = None
+            print("Your user name is either duplicated or un-available.")
+            print("Please enter a new/available to login to server:")
+        #server_parameter = server_json["parameter"]
 
     receiving_thread = threading.Thread(target=receiving_message, args=())
     sending_thread = threading.Thread(target=sending_message, args=())
--- server.py
+++ server.py
@@ -7,17 +7,120 @@ import configparser
 import threading
 
 
-def receiving_message(connection, client_address):
-    while True:
-        # Keep looking and receiving JSON message from client until receiving "QUITTING" command
-        # If command from client is "QUITTING", then the "response" element is "QUITTING" and "parameter" element is empty.
-        client_data = connection.recv(1024)
-        client_data = client_data.decode(encoding="utf-8")
+ip_user = {}
+active_users = {}
 
+# communication json type:
+LOGIN = "login"
+LIST_USERS = "list_users"
+BROADCAST = "broadcast"
+GROUP_SENDING = "group_sending"
+DIRECT_MESSAGE = "direct_message"
+
+SUCCESS = "success"
+FAIL = "fail"
+
+
+def group_sending(client_json):
+    message_type = client_json["type"]
+    sender = client_json["sender"]
+    message_content = client_json["content"]
+
+    for user in active_users:
+        connection = active_users[user]
+
+        message = {"type": message_type,
+                   "status": SUCCESS,
+                   "sender": sender,
+                   "content": message_content}
+
+        message_json = json.dumps(message)
+        message_json = message_json.encode(encoding="utf-8")
+        connection.send(message_json)
+
+
+def receiving_message(connection, client_ip):
+
+    while True:
+        # Keep looking and receiving JSON message from client
+        client_data = connection.recv(1024)
+
+        # client has closed the connection!!!
+        if not client_data:
+            # remove client ip, username, and connection from ip_user and active_users
+            username = ip_user[client_ip]
+            active_users.pop(username)
+            ip_user.pop(client_ip)
+            break
+
+        client_data = client_data.decode(encoding="utf-8")
         print(f"Receive from {client_address}: {client_data}")
 
+        client_json = json.loads(client_data)
+        message_type = client_json["type"]
 
-def sending_message(connection, client_address):
+        if message_type == LOGIN:
+            sender = client_json["sender"]
+            message_content = client_json["content"]
+
+            current_user = active_users.get(sender)
+            if current_user is None:
+                ip_user[client_ip] = sender
+                active_users[sender] = connection
+
+                message = {"type": message_type,
+                           "status": SUCCESS,
+                           "sender": "Server",
+                           "content": "You are successfully login to server."}
+
+            else:
+                message = {"type": message_type,
+                           "status": FAIL,
+                           "sender": "Server",
+                           "content": "The username has already been existed in the server."}
+
+            message_json = json.dumps(message)
+            message_json = message_json.encode(encoding="utf-8")
+            connection.send(message_json)
+
+        elif message_type == LIST_USERS:
+            users = []
+            for user in active_users:
+                users.append(user)
+
+            message = {"type": message_type,
+                       "status": SUCCESS,
+                       "sender": "Server",
+                       "content": users}
+
+            message_json = json.dumps(message)
+            message_json = message_json.encode(encoding="utf-8")
+            connection.send(message_json)
+
+        elif message_type == GROUP_SENDING:
+            group_sending(client_json=client_json)
+
+        elif message_type == DIRECT_MESSAGE:
+            try:
+                sender = client_json["sender"]
+                receiver = client_json["receiver"]
+                message_content = client_json["content"]
+
+                receiver_connection = active_users[receiver]
+
+                message = {"type": message_type,
+                           "status": SUCCESS,
+                           "sender": sender,
+                           "content": message_content}
+
+                message_json = json.dumps(message)
+                message_json = message_json.encode(encoding="utf-8")
+                receiver_connection.send(message_json)
+            except:
+                print("Error occurs")
+
+
+def server_notification():
     for line in sys.stdin:
         line = line.strip()
         line = line.encode(encoding="utf-8")
@@ -25,23 +128,34 @@ def sending_message(connection, client_address):
 
 
 if __name__ == '__main__':
+    server_port = 8888
+    print(f"Server is running at: ")
+    print(f"Host Name: {socket.gethostname()}")
+    print(f"Host IP: {socket.gethostbyname(socket.gethostname())}")
+    print(f"Host Port: {server_port}")
     # Set up socket
-    server_address = ("172.27.67.88", 8888)
+    server_address = ("127.0.0.1", server_port)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(server_address)
     server.listen(5)
+
+    sending_thread = threading.Thread(target=server_notification, args=())
+    sending_thread.start()
 
     while True:
         # Wait for an incoming connection.
         # Return a new socket representing the connection, and the address of the client.
         connection, client_address = server.accept()
 
-        print(f"Connection accepted from {client_address}")
-        logging.info(f"Connection accepted from {client_address}")
+        client_ip = client_address[0]
+        client_port = client_address[1]
+        print(f"Connection accepted from {client_ip} : {client_port}")
+        print(f"Connection accepted from {client_ip} : {client_port}")
 
-        receiving_thread = threading.Thread(target=receiving_message, args=(connection, client_address))
-        sending_thread = threading.Thread(target=sending_message, args=(connection, client_address))
+        ip_user[client_ip] = None
+
+        receiving_thread = threading.Thread(target=receiving_message, args=(connection, client_ip))
 
         receiving_thread.start()
-        sending_thread.start()
+
